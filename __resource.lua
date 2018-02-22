c3d7b26209f1c4da4fa16ae266b2834e66d0e8db
ElPumpo <samo_kol@hotmail.com>
2018-02-22 22:34:31 +0100

    Added Swedish translation

--- __resource.lua
+++ __resource.lua
@@ -11,6 +11,7 @@ server_scripts {
   'locales/en.lua',
   'locales/fr.lua',
   'locales/es.lua',
+  'locales/sv.lua',
   '@mysql-async/lib/MySQL.lua',
   'config.lua',
   'server/main.lua'
@@ -23,6 +24,7 @@ client_scripts {
   'locales/en.lua',
   'locales/fr.lua',
   'locales/es.lua',
+  'locales/sv.lua',
   'config.lua',
   'client/main.lua'
 }
